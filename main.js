758c22c20098786dd0d8ca1d66256628937f6bb3
Woori <rose5307@naver.com>
2022-03-11 19:11:38 +0900

    game refactoring - timer and game status

--- main.js
+++ main.js
@@ -2,19 +2,30 @@ const gameField = document.querySelector('.game-field');
 const sectionRect = gameField.getBoundingClientRect();
 const count = document.querySelector('.game-score');
 const resultText = document.querySelector('.result-text');
-const btnRestart = document.querySelector('.btn-restart');
+
 const btnStart = document.querySelector('.game-button');
 const countDownTimer = document.querySelector('.game-timer');
+
+const gamePopup = document.querySelector('.pop-up');
+const gamePopupText = document.querySelector('.pop-up-text');
+const gamePopupRestartBtn = document.querySelector('.pop-up-restart');
 
 const CARROT_SIZE = 80;
 const CARROT_COUNT = 5;
 const BUG_COUNT = 5;
+const GAME_DURATION_SEC = 10;
 let carrotCount = 0;
 let started = false;
 let timer = undefined;
 
+const carrotSound = new Audio('sound/carrot_pull.mp3');
+const alertSound = new Audio('sound/alert.mp3');
+const bgSound = new Audio('sound/bg.mp3');
+const winSound = new Audio('sound/game_win.mp3');
+const bugSound = new Audio('sound/bug_pull.mp3');
 
 const initGame = () => {
+    carrotCount = 0;
     gameField.innerHTML = '';
     count.innerText = CARROT_COUNT;
     addItem('carrot', 5, 'img/carrot.png');
@@ -43,103 +54,123 @@ const randomNumber = (min, max) => {
     return Math.random() * (max - min) + min;
 }
 
-//당근 세는 함수
-const onCountingCarrot = () =>{
-    carrotCount++;
-    count.innerText = carrotCount;
-    if(carrotCount == 10){
-        //게임오버 창 띄우기 : you win으로!
-        onGameOver('win');
+
+btnStart.addEventListener('click', () => {
+    if(started){
+        showPopup('Replay?');
+        stopGame();
+    }
+    else{
+        removePopup();
+        startGame();
+    }
+});
+
+gamePopupRestartBtn.addEventListener('click', () =>{
+    removePopup();
+    startGame();
+});
+
+const onFieldClick = (event) => {
+    if(!started){
+        return;
+    }
+    const target = event.target;
+    if(target.matches('.carrot')){
+        playSound(carrotSound);
+        event.target.remove();
+        // 카운팅하기
+        onCountingCarrot();
+        if(carrotCount === CARROT_COUNT){
+            finishGame(true);
+        }
+    }
+    else if(target.matches('.bug')){
+        // 게임오버 : you lose로!
+        finishGame(false);
     }
 }
 
-//타이머 함수
-const getCountDownTimer = (status) => {
-    let secondRemaining = 10;
-    let countDownInterval = setInterval(()=>{
-        if(status == 'play'){
+const playSound = (sound) => {
+    sound.currentTime = 0;
+    sound.play();
+}
+
+const stopSound = (sound) => {
+    sound.pause();
+}
+
+const finishGame = (win) => {
+    started = false;
+    stopSound(bgSound);
+    if(win){
+        playSound(winSound);
+    }
+    else{
+        playSound(bugSound);
+    }
+    stopGameTimer();
+    hideGameButton();
+    showPopup(win?'You Won' : 'You Lose')
+}
+
+const onCountingCarrot = () =>{
+    carrotCount++;
+    count.innerText = CARROT_COUNT - carrotCount;
+}
+
+const startGame = () => {
+    started = true;
+    initGame();
+    showStopButton();
+    showTimerAndScore();
+    startGameTimer();
+    playSound(bgSound);
+    btnStart.style.visibility = 'visible';
+}
+const stopGame = () => {
+    started = false;
+    playSound(alertSound);
+    stopSound(bgSound);
+    stopGameTimer();
+}
+
+const startGameTimer = () => {
+    let secondRemaining = GAME_DURATION_SEC;
+    countDownTimer.textContent = `0:${secondRemaining}`;
+    timer = setInterval(()=>{
+        if(started){
             countDownTimer.textContent = `0:${secondRemaining}`;
             secondRemaining--;
-            console.log(secondRemaining);
-        }
-        else if(status == 'pause'){
-            clearInterval(countDownInterval);
         }
         if(secondRemaining < 0){
-            clearInterval(countDownInterval);
-            onGameOver('lose');
+            clearInterval(timer);
+            finishGame(false);
         }
     }, 1000);
 }
 
-//클릭하면 처리하는 함수
-const onClickItem = (event) => {
-    if(event.target.classList[0] == 'carrot'){
-        carrots.removeChild(event.target);
-        // 카운팅하기
-        onCountingCarrot();
-    }
-    else if(event.target.classList[0] == 'bug'){
-        // 게임오버 : you lose로!
-        onGameOver('lose');
-    }
+const stopGameTimer = () => {
+    hideGameButton();
+    clearInterval(timer);
 }
 
-//뿌려진 애들 다 지우는 거
-const removeAllItem = () =>{
-    while(bugs.firstChild){
-        bugs.removeChild(bugs.firstChild);
-    }
-    while(carrots.firstChild){
-        carrots.removeChild(carrots.firstChild);
-    }
-}
+const hideGameButton = () => {
+    btnStart.style.visibility = 'hidden';
+};
 
-//게임오버 처리
-const onGameOver = (status) => {
-    if(status == 'win'){
-        resultText.innerText = 'You Win!';
-    }
-    else if(status == 'lose'){
-        resultText.innerText = 'You lose';
-    }
-    btnRestart.addEventListener('click', onInitGame);
-}
-
-//게임 일시정지
-const onGamePauseToggle = () => {
-    if(timerStatus == 'play'){
-        timerStatus = 'pause';
-    }
-    else{
-        timerStatus = 'play';
-    }
-    getCountDownTimer(timerStatus);
-}
-
-//게임 초기화 부분. 나중에는 스타트 버튼 누르면 나오는 거로 바꿀 것.
-btnStart.addEventListener('click', () => {
-    if(started){
-        stopGame();
-    }
-    else{
-        startGame();
-    }
-    started = !started;
-});
-
-const startGame = () => {
-    initGame();
-    showStopButton();
-    showTimerAndScore();
-}
-const stopGame = () => {
-
-}
 const showStopButton = () => {
     const icon = btnStart.querySelector('.fa-play');
     icon.classList.add('fa-stop');
     icon.classList.remove('.fa-play');
+}
+
+const showPopup = (message) => {
+    gamePopupText.textContent = message;
+    gamePopup.classList.remove('pop-up-hide');
+}
+const removePopup = () => {
+    gamePopup.classList.add('pop-up-hide');
 }
 
 const showTimerAndScore = () => {
@@ -149,3 +180,4 @@ const showTimerAndScore = () => {
 
 // bugs.addEventListener('click', onClickItem);
 // carrots.addEventListener('click', onClickItem);
+gameField.addEventListener('click', onFieldClick);
